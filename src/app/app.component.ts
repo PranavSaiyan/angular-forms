521e6ea1c7c2f89371f10e0c2907719622d225cd
PranavSaiyan <pranavsupersaiyan10@gmail.com>
2019-04-13 18:21:16 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  onSubmit(form : NgForm){
+    console.log(form);
+  }
 }
