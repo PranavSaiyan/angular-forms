06940522f32c4e27ba6e8e6ffc1d937da524c137
PranavSaiyan <pranavsupersaiyan10@gmail.com>
2019-04-25 17:41:43 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,20 @@ import { NgForm} from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  @ViewChild('f') submitForm: NgForm;
+  @ViewChild('f') submitForm;
   @ViewChild('mail') email;
   warn = false;
   answer = '';
   defaultoption = 'book';
   genders = ['Male','Female'];
+  formUser = {
+    name:'',
+    email:'',
+    password:'',
+    gender:'',
+    question:'',
+    answer:''
+  }
   suggestUsername() {
 //     this.submitForm.setValue({
 // Answer: "leo",
@@ -27,6 +35,18 @@ export class AppComponent  {
       });
   }
   onSubmit(){
+    this.formUser.name = this.submitForm.form.value.user;
+
+    this.formUser.email = this.submitForm.form.value.email;
+
+    this.formUser.gender = this.submitForm.form.value.gender;
+
+    this.formUser.password = this.submitForm.form.value.pass;
+
+    this.formUser.question = this.submitForm.form.value.list;
+
+    this.formUser.answer = this.submitForm.form.value.Answer;
+    console.log(this.formUser);
     console.log(this.submitForm);
     console.log(this.email);
   }
